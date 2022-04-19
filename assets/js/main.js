76e3ca7f69e1d0d885917f5e9cd3dc7eb0d9af22
StephanPagel <stephan.pagel84@gmail.com>
2022-04-19 14:34:53 +0200

    cleaned

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,6 @@
-console.log("it works");
-
 let inputYear = document.getElementById('input_year');
 let output = document.getElementById('form_output');
 
 function age() {
-    console.log("function works");
-    console.log(inputYear.value);
     output.innerHTML = 2022 - inputYear.value;
 }
